92e8d46d7c2b7d40dad92514b752a265ac086c57
ryonext <ryonext.s@gmail.com>
2016-10-04 18:34:56 +0900

    Initial Commit

---
+++ PictureSharingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PictureSharingApp
+//
+//  Created by Shimamoto, Ryoji on 2016/10/04.
+//  Copyright © 2016年 Shimamoto, Ryoji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
