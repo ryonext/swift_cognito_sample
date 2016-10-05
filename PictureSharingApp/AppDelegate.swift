506ada1181b11f7e0582f714d862681c3f89cb68
ryonext <ryonext.s@gmail.com>
2016-10-05 19:31:58 +0900

    Implement by tutorial, but it does not works.

--- PictureSharingApp/AppDelegate.swift
+++ PictureSharingApp/AppDelegate.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
 import AWSCore
+import AWSCognito
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: .apNortheast1, identityPoolId: "")
+        let configuration = AWSServiceConfiguration(region: .apNortheast1, credentialsProvider: credentialsProvider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        var syncClient = AWSCognito.default()
+        syncClient.openOrCreateDataset("myDataset")
+        
+        
         return true
     }
 
